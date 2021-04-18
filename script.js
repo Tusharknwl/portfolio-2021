a0e9eb39d905c79797bcee6399d9dec0bdeca49f
Tushar <>
2021-04-18 20:10:46 +0000

    19 Apr 21

--- script.js
+++ script.js
@@ -51,6 +51,6 @@ var TxtType = function(el, toRotate, period) {
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #3DCFD3;}";
+        css.innerHTML = ".typewrite > .wrap { }";
         document.body.appendChild(css);
     };
